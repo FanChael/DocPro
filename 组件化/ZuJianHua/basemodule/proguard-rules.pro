f823af8219f6098194f5524a18b552f907d077bd
huanglei <huangleililei@gmail.com>
2019-10-03 18:00:47 +0800

    提交组件化实践工程

---
+++ 组件化/ZuJianHua/basemodule/proguard-rules.pro
@@ -0,0 +1,30 @@
+# Add project specific ProGuard rules here.
+# You can control the set of applied configuration files using the
+# proguardFiles setting in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+# Uncomment this to preserve the line number information for
+# debugging stack traces.
+#-keepattributes SourceFile,LineNumberTable
+
+# If you keep the line number information, uncomment this to
+# hide the original source file name.
+#-renamesourcefileattribute SourceFile
+-keep public class com.alibaba.android.arouter.routes.**{*;}
+-keep public class com.alibaba.android.arouter.facade.**{*;}
+-keep class * implements com.alibaba.android.arouter.facade.template.ISyringe{*;}
+
+# If you use the byType method to obtain Service, add the following rules to protect the interface:
+-keep interface * implements com.alibaba.android.arouter.facade.template.IProvider
+
+# If single-type injection is used, that is, no interface is defined to implement IProvider, the following rules need to be added to protect the implementation
+# -keep class * implements com.alibaba.android.arouter.facade.template.IProvider
---
+++ 组件化/ZuJianHua/personal/src/main/java/com/skl/personal/PersonalApplication.java
@@ -0,0 +1,58 @@
+package com.skl.personal;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.skl.basemodule.AppConfig;
+import com.skl.basemodule.CompomentsService;
+import com.skl.basemodule.common_interface.IAppComponent;
+
+/**
+ * 单独运行时需要的话可以配置到AndroidManifest，不需要就不要配置，不然运行报错
+ */
+public class PersonalApplication extends Application implements IAppComponent {
+    /**
+     * 单独作为Application时会走该方法
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO 单独运行时没有需要可以不用调用
+        initialize(this);
+    }
+
+    /**
+     * App的Application注册组件时会调用initialize方法!
+     *
+     * @param app
+     */
+    @Override
+    public void initialize(Application app) {
+        // 注册自己到组件服务
+        CompomentsService.setiAppComponentHashMap(AppConfig.PAGE_TYPE.PERSONAL, this);
+    }
+
+    /**
+     * 启动自己
+     *
+     * @param context
+     * @param extra
+     */
+    @Override
+    public void launch(Context context, String extra) {
+        Intent intent = new Intent(context, PersonalActivity.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            Bundle bundle = new Bundle();
+            if (null != extra && !extra.equals(""))
+                bundle.putString("extra_data", extra);
+            context.startActivity(intent, bundle);
+        } else {
+            if (null != extra && !extra.equals(""))
+                intent.putExtra("extra_data", extra);
+            context.startActivity(intent);
+        }
+    }
+}
